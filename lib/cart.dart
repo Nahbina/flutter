b73cde15727d1730d9b3df1a4ebf4dbc2ffc5d29
Nahbina <nabinamagat@gmail.com>
2023-08-30 18:40:07 +0545

    counter app

---
+++ lib/cart.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CartScreen extends StatelessWidget {
+  const CartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network("https://th.bing.com/th/id/OIP.cOMrhY4XasA_a6TeYvkJ5AAAAA?pid=ImgDet&rs=1");
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+           
+            children: [
+              Container(
+                color: Colors.red,
+                width: 100,
+                
+              ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          color: Colors.yellow,
+                          height: 100,
+                          width: 100,
+                        ),
+                        Container(
+                          color: Colors.green,
+                          height: 100,
+                          width: 100,
+                        ),
+                      ],
+                    ),
+                    
+                  
+    
+            Container(
+              color: Colors.blue,
+              width: 100,
+            
+            
+           ) ],
+        
+          ),
+        ));}
+}
